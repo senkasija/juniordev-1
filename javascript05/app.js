5ee3d816614fa758192c766fe2b7d50c5ae0ce1c
Ivan Jevđović <ivan.jevdjovic@gmail.com>
2020-05-08 19:50:59 +0200

    Dopuna operatorima

--- javascript05/app.js
+++ javascript05/app.js
@@ -1,13 +1,14 @@
 
-
+//funkcija koja određuje šta će da se desi kada se aktivira dugme
 function racunaj() {
+    //deponuje vrednosti polja za broj
     var prvaCifra = document.getElementById("prviBroj").value;
     var drugaCifra = document.getElementById("drugiBroj").value;
-
+    //deponuje vrednosti računske operacije iz objekta select
     var digitron = document.getElementById("digitron");
     var operacija = digitron.options[digitron.selectedIndex].value;  
     
-
+    //funkcija koja izvršava operacije i vraća vrednost operacije
     function rezultat(x, y){
        
         if (operacija == "plus") 
@@ -28,7 +29,9 @@ function racunaj() {
         }
         return ovodaseUpise;
     }
+    //pozivanje funkcije za izvršavanje operacije 
     document.getElementById("rezultat").innerHTML = rezultat(prvaCifra, drugaCifra);
+    //varijabla koja deponuje vrednost rezultata
     var igra = document.getElementById("rezultat").value;
     
     /*------------------------------ OPERATORI --------------------------------*/
@@ -40,8 +43,29 @@ function racunaj() {
     console.log(typeof igra);
     console.log(!igra);
     console.log(delete igra);
-   
+  
 }
+
+var i = 0;
+console.log("i: "+ i);
+console.log("i++: "+ i++);
+console.log("i: "+ i);
+console.log("++i: "+ ++i);
+console.log("i: "+ i);
+console.log("--i: "+ --i);
+console.log("i: "+ i);
+var y = i;
+console.log("i--: "+ i--);
+console.log("y: " + i);
+
+if (i && y){
+    console.log("true and false is false");
+}
+if (i || y){
+    console.log("true ili false is true");
+}
+
+
 
 
 
