6a054a0e7f8bbd2ba8a0f623c67762c4729f6068
senkasija <ivan.jevdjovic@gmail.com>
2020-05-11 19:52:34 +0200

    for petlja

---
+++ javascript06/app.js
@@ -0,0 +1,47 @@
+/*
+** najčešće se koristi
+** bolja za nizove sa utvrđenim indeksom, te for petlju koristimo kada znamo koliko puta petlja treba da se izvrši
+** preglednija jer su i početna vrednost, uslov i inkrement na jednom mestu
+
+*/
+
+var x = "ljutih gusara";
+var y = "Jedan ";
+
+var gusari = [10, 9, 8, 7, 6, 5, 4, 3, 2, 1, "nijedan"];
+
+for (i=0; i < gusari.length; ++i){
+
+
+    var kolikoGusara = gusari[i];
+    console.log(kolikoGusara);
+    var  ostatakGusara = "Ostalo ih " + --gusari[i];
+    pesma = [
+        `${kolikoGusara} ${x}<br>Došlo u moj krevet<br>${y} pao s kreveta<br>${ostatakGusara}<p>`,
+        `${kolikoGusara} ${x}<br>Još ne znaju ko sam<br>Jednog sam uspavao<br>${ostatakGusara}<p>`,
+        `${kolikoGusara} ${x}<br>Ja ih oštro gledam<br>${y}pao u nesvest<br>${ostatakGusara}<p>`,
+        `${kolikoGusara} ${x}<br>Pobeglo na brest<br>${y}pao na glavu<br>${ostatakGusara}<p>`,
+        `${kolikoGusara} ${x}<br>Zbrisalo u svet<br>${y}se izgubio<br>${ostatakGusara}<p>`,
+        `${kolikoGusara} ${x}<br>Lete ko leptiri<br>${y}pao u bunar<br>${ostatakGusara}<p>`,
+        `${kolikoGusara} ${x}<br>Bes u njima vri<br>${y}puko od muke<br>${ostatakGusara}<p>`,
+        `${kolikoGusara} ${x}<br>A protiv njih ja<br>Jednog sam zviznuo<br>${ostatakGusara}<p>`,
+        `${kolikoGusara} ${x}<br>A pogled im ledan<br>${y}se okliznuo<br>${ostatakGusara}<p>`,
+        `${kolikoGusara} ${x}<br>Postao je medan<br>Prosto se istopio<br>${ostatakGusara}<p>`,
+        "U svakom pogledu<br>Prošla me je strava<br>Sad je sve u redu<br>Može da se spava<p>" 
+    ]
+    document.write(pesma[i]);
+}
+/*for (var indeks in gusari)  {
+    console.log(indeks + ": " + gusari[indeks]);
+}
+*/
+/*var pesmaObjekat = {
+    prvaStrofa: `${kolikoGusara} ${x}<br>Došlo u moj krevet<br>${y} pao s kreveta<br>${ostatakGusara}<p>`,
+    drugaStrofa: `${kolikoGusara} ${x}<br>Još ne znaju ko sam<br>Jednog sam uspavao<br>${ostatakGusara}<p>`,
+    trecaStrofa: `${kolikoGusara} ${x}<br>Ja ih oštro gledam<br>${y}pao u nesvest<br>${ostatakGusara}<p>`
+}*/
+/*for (var indeks1 in pesma)  {
+    console.log(indeks1 + ": " + pesma[indeks1]);   
+}*/
+
+gusari.forEach(element =>console.log(element));
